e42df855c02bd1853ce8f7a70230eaaac6818868
Fer <pxocktopus@gmail.com>
2026-01-20 21:11:31 +0100

    añadido movimiento de la bola y colisiones

--- figura_class.py
+++ figura_class.py
@@ -7,15 +7,34 @@ class Raqueta:
                 self.color= color
                 self.w= w
                 self.h= h
-
+            
         def dibujar(self,screen):
-                pg.draw.rect(screen,self.color,(self.pos_x,self.pos_y,self.w,self.h))
+                pg.draw.rect(screen,self.color,(self.pos_x,(self.pos_y-self.h//2),self.w,self.h))
+        
+        def mover(self,teclado_arriba,teclado_abajo):
+            teclado = pg.key.get_pressed()
+            if teclado[teclado_arriba]== True and self.pos_y >=0+(self.h//2):
+                self.pos_y = self.pos_y - 1
+            if teclado[teclado_abajo]== True and self.pos_y <= 600-(self.h//2):
+                self.pos_y = self.pos_y + 1
 class Pelota:
         def __init__(self,pos_x,pos_y,color=(255,255,255),radio=20):
                 self.pos_x= pos_x
                 self.pos_y= pos_y
                 self.color= color
                 self.radio= radio
+                self.vx= 1
+                self.vy= 1
                 
         def dibujar(self,screen):
-                pg.draw.circle(screen,self.color,(self.pos_x,self.pos_y), self.radio)        
+                pg.draw.circle(screen,self.color,(self.pos_x,self.pos_y), self.radio)   
+
+        def mover(self,xmax,ymax):
+            self.pos_x = self.pos_x + self.vx
+            self.pos_y = self.pos_y + self.vy
+            if self.pos_x >= xmax-(self.radio) or self.pos_x <=0 +(self.radio):
+                self.vx = self.vx*-1
+            if self.pos_y >= ymax -(self.radio) or self.pos_y <=0 +(self.radio):
+                self.vy = self.vy*-1
+               
+        
--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import pygame as pg
 from figura_class import Raqueta,Pelota
 pg.init()
 
-raqueta_1 = Raqueta(0,300-60)
-raqueta_2 = Raqueta(800-20,300-60)
+raqueta_1 = Raqueta(0,300)
+raqueta_2 = Raqueta(800-20,300)
 pelota= Pelota(400,300)
 
 pantalla = pg.display.set_mode((800,600))
 pg.display.set_caption("PONG")
+tasa_refresco= pg.time.Clock()
 
 game_over= True
 
 while game_over:
+    valor_tasa= tasa_refresco.tick(440)
     for eventos in pg.event.get():
         if eventos.type == pg.QUIT:
             game_over = False
+
+    pelota.mover(800,600)
+
     pantalla.fill((0,0,0))
     pg.draw.line(pantalla,(255,255,255),(400,0),(400,600),width=10)
 
@@ -22,6 +27,9 @@ while game_over:
     raqueta_1.dibujar(pantalla)
     raqueta_2.dibujar(pantalla)
     pelota.dibujar(pantalla)
+
+    raqueta_1.mover(pg.K_w,pg.K_s)
+    raqueta_2.mover(pg.K_UP,pg.K_DOWN)
     pg.display.flip()
 
 pg.quit()
